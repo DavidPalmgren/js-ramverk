58153bd2222f2160a342de9e8b38e3bd4b413e56
DavidPalmgren <swemultigamers@gmail.com>
2024-10-31 05:46:04 +0100

    fixing bth settings

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx';
 import Document from './routes/Document.tsx';
 import CreateAccount from './routes/CreateAccount.tsx';
-import Login from './routes/Login.tsx';
 import AllDocuments from './routes/AllDocuments.tsx';
-import './index.css';
 import DocumentInvite from './routes/DocumentInvite.tsx';
+import Login from './routes/Login.tsx';
+import './index.css';
 
 import CodeEditor from './routes/CodeEditor.tsx';
+
+const NotFound = () => <div>404 this will load but nothing else</div>;
+const basename = "/~dapa22/editor/";
 
 const router = createBrowserRouter([
   {
@@ -41,7 +44,11 @@ const router = createBrowserRouter([
     path: 'code/:id',
     element: <CodeEditor />,
   },
-]);
+  {
+    path: "*", // This will catch all other routes
+    element: <NotFound/>
+  }
+], {basename});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
